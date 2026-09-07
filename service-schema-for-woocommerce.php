912458f4b865491b17480000f76d7d6382e9525c
Bryan Headrick <info@catmanstudios.com>
2026-09-06 20:39:29 -0400

    initial commit

---
+++ service-schema-for-woocommerce.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Service Schema For Woocommerce
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     service-schema-for-woocommerce
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Service_Schema_For_Woocommerce
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Service_Schema_For_Woocommerce
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
