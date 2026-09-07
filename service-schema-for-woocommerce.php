99bb45d21899c33614d8642e6c5f7a03ec09334b
Bryan Headrick <info@catmanstudios.com>
2026-09-07 07:49:26 -0400

    chore: replace scaffold placeholders with real plugin metadata

--- service-schema-for-woocommerce.php
+++ service-schema-for-woocommerce.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:     Service Schema For Woocommerce
  * Plugin URI:      https://bryanheadrick.com
- * Description:     PLUGIN DESCRIPTION HERE
+ * Description:     Adds a "Service" option to WooCommerce products and outputs schema.org Service structured data instead of Product for those items.
  * Author:          bryanheadrick
  * Author URI:      https://bryanheadrick.com
  * Text Domain:     service-schema-for-woocommerce
@@ -11,6 +11,10 @@
  * Version:         0.1.0
  *
  * @package         Service_Schema_For_Woocommerce
+ *
+ * Requires Plugins: woocommerce
+ * License:          GPL v2 or later
+ * License URI:      https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 // Your code starts here.
